4af1c6f8a98b5bae7bd657176a17d59a285dde9e
Liviu G <ic01oblivionp@gmail.com>
2020-03-27 15:46:01 +0200

    Add JS file for auth forms script src

--- private/includes/auth/auth_footer.php
+++ private/includes/auth/auth_footer.php
@@ -1,4 +1,5 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+<script src="<?php echo url_for('/assets/js/register.js'); ?>"></script>
 </body>
 </html>
