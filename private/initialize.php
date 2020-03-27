d23cfcdef45abafaa16304d772cc150d0a9ebd96
Liviu G <ic01oblivionp@gmail.com>
2020-03-27 13:34:24 +0200

    Initial Commit
    1. Start output buffering and sessions
    2. define global variables that separates private section from public parts
    3. define WWW_ROOT global variable that will be used in the function url_for() that will make linking assets easier
    4. add section for config/ files to be included
    5. create an autoload for all classes
    6. assing db connection that can be used in all the project files
    
    This file "initialize.php" will connect everything from models/controllers to the view section, without including any classes, the only file included in the view pages will be only this file "initialize.php"

---
+++ private/initialize.php
@@ -0,0 +1,42 @@
+<?php 
+
+// Start output buffering
+ob_start();
+
+// Start Sessions
+session_start();
+
+// Define variables that will help with authorization, separating private folder from public folder
+// Ex: instead of required_once("../../example.php") the secure url will be required_once(SHARED_PATH . "example.php")
+define('PRIVATE_PATH', dirname(__FILE__));
+define('PROJECT_PATH', dirname(PRIVATE_PATH));
+define('PUBLIC_PATH', PROJECT_PATH . '/public');
+define('SHARED_PATH', PRIVATE_PATH . '/shared/');
+
+// Get url root to make it easier to link files
+$public_end = strpos($_SERVER['SCRIPT_NAME'], '/public') + 7;
+$doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
+define('WWW_ROOT', $doc_root);
+
+// Required files that can be used everywhere in the project
+require_once ('config/functions.php');
+
+// Require all class files to this file 'initialize.php' so that you don't need to link class files in every view pages
+// you will only require to link this file to all view pages (all it does is autoload classes)
+foreach (glob('models/*.php') as $file) {
+    require_once ('$file');
+}
+
+// find all classes in the models folder and autoload it
+function my_autoload($class) {
+    if (preg_match('/\A\w+\Z/', $class)) {
+        include ('models/' . $class . '.php');
+    }
+}
+
+spl_autoload_register('my_autoload');
+
+// Instantiate DB & connect
+$database = new Database();
+$db = $database->connect();
+
