886a5df3d3271d412c1e27fb533df9a49f035cc8
Liviu G <ic01oblivionp@gmail.com>
2020-03-28 16:27:56 +0200

    Edit User.php file
    1. line formatting
    2. change public function create() query "SET" into "VALUES"
    3. add query that gets a user data based on email (used in login_handler.php)
    4. add query that reopen an account based on the user_closed property (if user_closed == yes, then account is closed, else is not); user is found by the id

--- private/models/User.php
+++ private/models/User.php
@@ -1,78 +1,86 @@
-<?php
+ <?php
 
-class User
-{
-    // DB prop
-    private $conn;
-    private $table = 'users';
-
-    // User prop
-    public $id;
-    public $username;
-    public $email;
-    public $password;
-    public $height = 0;
-    public $weight = 0;
-    public $gender;
-    public $profile_pic;
-    public $user_closed;
-    public $created_at;
-    public $updated_at;
-
-    // DB __constructor
-    public function __construct($db)
+    class User
     {
-        $this->conn = $db;
-    }
+        // DB prop
+        private $conn;
+        private $table = 'users';
 
-    // Hash password
-    public function hash_password($pass)
-    {
-        return $this->password = password_hash($pass, PASSWORD_BCRYPT);
-    }
+        // User prop
+        public $id;
+        public $username;
+        public $email;
+        public $password;
+        public $height = 0;
+        public $weight = 0;
+        public $gender;
+        public $profile_pic;
+        public $user_closed;
+        public $created_at;
+        public $updated_at;
 
-    // Check if email exists
-    public function check_email_unique($email)
-    {
-        // Create query
-        $query = 'SELECT * FROM ' . $this->table . ' WHERE email = :email LIMIT 0,1';
+        // DB __constructor
+        public function __construct($db)
+        {
+            $this->conn = $db;
+        }
 
-        // Prepare statement
-        $stmt = $this->conn->prepare($query);
+        // Hash password
+        public function hash_password($pass)
+        {
+            return $this->password = password_hash($pass, PASSWORD_BCRYPT);
+        }
 
-        // Bind email
-        $stmt->bindParam(':email', $email);
+        // Check if email exists
+        public function check_email_unique($email)
+        {
+            // Create query
+            $query = 'SELECT * FROM ' . $this->table . ' 
+                    WHERE 
+                        email = :email 
+                    LIMIT 0,1
+                ';
 
-        // Execute query
-        $stmt->execute();
+            // Prepare statement
+            $stmt = $this->conn->prepare($query);
 
-        return $stmt;
-    }
+            // Bind email
+            $stmt->bindParam(':email', $email);
 
-    // Check if username exists
-    public function check_username_unique($username)
-    {
-        // Create query
-        $query = 'SELECT * FROM ' . $this->table . ' WHERE username= :username LIMIT 0,1';
+            // Execute query
+            $stmt->execute();
 
-        // Prepare statement
-        $stmt = $this->conn->prepare($query);
+            return $stmt;
+        }
 
-        // Bind username
-        $stmt->bindParam(':username', $username);
+        // Check if username exists
+        public function check_username_unique($username)
+        {
+            // Create query
+            $query = 'SELECT * FROM ' . $this->table . ' 
+                    WHERE 
+                        username= :username 
+                    LIMIT 0,1
+                ';
 
-        // Execute query
-        $stmt->execute();
+            // Prepare statement
+            $stmt = $this->conn->prepare($query);
 
-        return $stmt;
-    }
+            // Bind username
+            $stmt->bindParam(':username', $username);
 
-    // Create user
-    public function create()
-    {
-        // Create query
-        $query = 'INSERT INTO ' . $this->table . '
-                    SET
+            // Execute query
+            $stmt->execute();
+
+            return $stmt;
+        }
+
+        // Create user
+        public function create()
+        {
+            // Create query
+            $query = 'INSERT INTO ' . $this->table . '
+                    VALUES
                         username= :username,
                         email= :email,
                         password= :password,
@@ -85,41 +93,85 @@ class User
                         updated_at= :updated_at
                 ';
 
-        // Prepare statement
-        $stmt = $this->conn->prepare($query);
+            // Prepare statement
+            $stmt = $this->conn->prepare($query);
 
-        // Clean data
-        $this->username = h(st($this->username));
-        $this->email = h(st($this->email));
-        $this->password = h(st($this->password));
-        $this->height = h(st($this->height));
-        $this->weight = h(st($this->weight));
-        $this->gender = h(st($this->gender));
-        $this->profile_pic = h(st($this->profile_pic));
-        $this->user_closed = h(st($this->user_closed));
-        $this->created_at = h(st($this->created_at));
-        $this->updated_at = h(st($this->updated_at));
+            // Clean data
+            $this->username = h(st($this->username));
+            $this->email = h(st($this->email));
+            $this->password = h(st($this->password));
+            $this->height = h(st($this->height));
+            $this->weight = h(st($this->weight));
+            $this->gender = h(st($this->gender));
+            $this->profile_pic = h(st($this->profile_pic));
+            $this->user_closed = h(st($this->user_closed));
+            $this->created_at = h(st($this->created_at));
+            $this->updated_at = h(st($this->updated_at));
 
-        // Bind data
-        $stmt->bindParam(':username', $this->username);
-        $stmt->bindParam(':email', $this->email);
-        $stmt->bindParam(':password', $this->password);
-        $stmt->bindParam(':height', $this->height);
-        $stmt->bindParam(':weight', $this->weight);
-        $stmt->bindParam(':gender', $this->gender);
-        $stmt->bindParam(':profile_pic', $this->profile_pic);
-        $stmt->bindParam(':user_closed', $this->user_closed);
-        $stmt->bindParam(':created_at', $this->created_at);
-        $stmt->bindParam(':updated_at', $this->updated_at);
+            // Bind data
+            $stmt->bindParam(':username', $this->username);
+            $stmt->bindParam(':email', $this->email);
+            $stmt->bindParam(':password', $this->password);
+            $stmt->bindParam(':height', $this->height);
+            $stmt->bindParam(':weight', $this->weight);
+            $stmt->bindParam(':gender', $this->gender);
+            $stmt->bindParam(':profile_pic', $this->profile_pic);
+            $stmt->bindParam(':user_closed', $this->user_closed);
+            $stmt->bindParam(':created_at', $this->created_at);
+            $stmt->bindParam(':updated_at', $this->updated_at);
 
-        // Execute query
-        if ($stmt->execute()) {
-            return true;
+            // Execute query
+            if ($stmt->execute()) {
+                return true;
+            }
+
+            // Print error if something went wrong
+            printf('Error: %s.\n', $stmt->error);
+
+            return false;
         }
 
-        // Print error if something went wrong
-        printf('Error: %s.\n', $stmt->error);
+        // get all user data based on email and password
+        public function read_user($email)
+        {
+            // Create query
+            $query = 'SELECT * FROM ' . $this->table . ' 
+                    WHERE 
+                        email= :email
+                    LIMIT 0,1
+                ';
 
-        return false;
+            // Prepare statement
+            $stmt = $this->conn->prepare($query);
+
+            // Bind data
+            $stmt->bindParam(':email', $email);
+
+            // Execute query
+            $stmt->execute();
+
+            return $stmt;
+        }
+
+        // get all user data based on email and if user account closed
+        public function reopen_user($uc, $id)
+        {
+            // Create query
+            $query = 'UPDATE ' . $this->table . ' 
+                    SET 
+                        user_closed = :user_closed 
+                    WHERE 
+                        id= :id
+                ';
+
+            // Prepare statement
+            $stmt = $this->conn->prepare($query);
+
+            // Bind data
+            $stmt->bindParam(':user_closed', $uc);
+            $stmt->bindParam(':id', $id);
+
+            // Execute query
+            $stmt->execute();
+        }
     }
-}
