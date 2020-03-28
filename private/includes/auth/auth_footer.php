453228b2c9414a6b169f7abc72f48bb991dbfbb0
Liviu G <ic01oblivionp@gmail.com>
2020-03-28 12:09:37 +0200

    Add JS script with php checking
    if register_submit is pressed and if the form is not completed (sent) the form will not display the login form yet

--- private/includes/auth/auth_footer.php
+++ private/includes/auth/auth_footer.php
@@ -1,5 +1,20 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 <script src="<?php echo url_for('/assets/js/register.js'); ?>"></script>
+
+<?php
+
+if (isset($_POST['register_submit'])) {
+    echo '
+            <script>
+                $(document).ready(function() {
+                    $("#login_form").hide();
+                    $("#register_form").show();
+                });
+            </script>
+        ';
+}
+
+?>
 </body>
 </html>
