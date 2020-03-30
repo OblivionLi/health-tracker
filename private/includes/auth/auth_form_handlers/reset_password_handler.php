7dfa019a100e34e8955fdb6019aa345b81361439
Liviu G <ic01oblivionp@gmail.com>
2020-03-30 16:19:29 +0300

    Add reset password handler file
    file checks if token in url exists, if not users are redirect to the login page
    check if reset_current_pass button is pressed if it is then connect to the DB
    then get both password and confirm new password and validate them
    if there are no errors
    query through password_resets table and find email by token and if it exist then assign email to assoc array
    hash the new password and update the updated_at date
    check if update_user query is true, if it is then delete all results from password_resets where the email is the same one that it is associated with the token
    then redirect_to login page if everything went right

---
+++ private/includes/auth/auth_form_handlers/reset_password_handler.php
@@ -0,0 +1,56 @@
+<?php
+
+$reset_password = "";
+$reset_password2 = "";
+$error = [];
+
+// grab token
+$token = h(st($_GET['token']));
+
+// check if token doesn't exist redirect back to login, else continue
+if (empty($token)) {
+    redirect_to('auth.php');
+} else {
+    if (isset($_POST['reset_current_pass']) && filter_has_var(INPUT_POST, 'reset_current_pass')) {
+        // Instantiate User object
+        $user = new User($db);
+
+        // Convert special characters and strip tags from reset_password form value
+        $reset_password = h(st($_POST['reset_password']));
+        $reset_password2 = h(st($_POST['reset_password2']));
+
+        // Check if reset_password and reset_password2 don't match or if they contain other characters than A-Z, 0-9
+        if ($reset_password != $reset_password2) {
+            array_push($error, "Your passwords do not match.");
+        } else {
+            if (preg_match('/[^A-Za-z0-9]/', $reset_password)) {
+                array_push($error, "Your password can only contain english characters or numbers.");
+            }
+        }
+
+        // Check if reset_password value is between 5 and 30 characters
+        if (strlen($reset_password) > 30 || strlen($reset_password) < 5) {
+            array_push($error, "Your password must be between 5 and 30 characters.");
+        }
+
+        // Check if there are no errors, if there aren't any then do the DB logic and register new user
+        if (empty($error)) {
+            // get email based on the token
+            $email_query = $user->read_password_reset($token);
+            $email = $email_query->fetch(PDO::FETCH_ASSOC);
+
+            if ($email) {
+                // hash the password typed in the field and assign values
+                $password = $user->hash_password($reset_password);
+                $email = $email['email'];
+                $updated_at = date('Y-m-d');
+
+                // update and redirect
+                if ($user->update_user($email, $password, $updated_at)) {
+                    $user->delete_password_reset($email);
+                    redirect_to('auth.php');
+                }
+            }
+        }
+    }
+}
