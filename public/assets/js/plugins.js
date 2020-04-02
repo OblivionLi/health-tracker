389a269798d8848edbe8252475f12c1407718348
Liviu G <ic01oblivionp@gmail.com>
2020-04-02 16:51:00 +0300

    Add JS script to show/hide forms

---
+++ public/assets/js/plugins.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $("#change-pass").click(function() {
+        $("#change-password").toggle("slow");
+    });
+
+    $("#close-acc").click(function() {
+        $("#close-account").toggle("slow");
+    });
+
+    $("#weight-height").click(function() {
+        $("#weight-height-content").slideDown("slow", function() {
+            $("#water-intake-content").slideUp("slow");
+        });
+        $('.weight-height').addClass('header');
+        $('.water-intake').removeClass('header');
+    });
+
+
+    $("#water-intake").click(function() {
+        $("#water-intake-content").slideDown("slow", function() {
+            $("#weight-height-content").slideUp("slow");
+        });
+        $('.water-intake').addClass('header');
+        $('.weight-height').removeClass('header');
+    });
+});
