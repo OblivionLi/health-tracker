68430cac683a21b58701b2a76251879cf995868b
Liviu G <ic01oblivionp@gmail.com>
2020-04-02 16:49:45 +0300

    Add strtoupper() to username SESSION var

--- private/includes/main_header.php
+++ private/includes/main_header.php
@@ -26,7 +26,7 @@
         <ul class="navbar-nav ml-auto">
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <?php echo $_SESSION['username'] ; ?>
+                <?php echo strtoupper($_SESSION['username']) ; ?>
             </a>
             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
               <a class="dropdown-item" href="<?php echo url_for('views/user/profile.php'); ?>">Profile</a>
