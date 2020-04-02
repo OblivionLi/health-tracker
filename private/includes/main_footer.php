49a61d9e4e2b1ed9f1481c23f338757eb8d69cc5
Liviu G <ic01oblivionp@gmail.com>
2020-04-02 14:22:35 +0300

    Add closing div and plugins.js file

--- private/includes/main_footer.php
+++ private/includes/main_footer.php
@@ -1,4 +1,6 @@
+</div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+<script src="<?php echo url_for('/assets/js/plugins.js'); ?>"></script>
 </body>
 </html>
