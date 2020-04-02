9d6221016cb6b584c4d6c7bce232e05c20c2587e
Liviu G <ic01oblivionp@gmail.com>
2020-04-02 14:23:12 +0300

    main_header additions
    check if user is logged, if not redirect to auth.php file
    add a container that holds the content and an universal navigation bar that displays the brand and the user name dropdown menu where he can choose to go to profile page or logout

--- private/includes/main_header.php
+++ private/includes/main_header.php
@@ -1,12 +1,39 @@
+<?php is_logged() ?>
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Health Tracker</title>
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="<?php url_for('assets/css/style.css'); ?>">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Health Tracker</title>
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?php echo url_for('assets/css/style.css'); ?>">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
+
 <body>
+
+  <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light navbar-header">
+      <a class="navbar-brand" href="#">Health-Tracker</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <?php echo $_SESSION['username'] ; ?>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="<?php echo url_for('views/user/profile.php'); ?>">Profile</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="<?php echo url_for('views/user/logout.php'); ?>">Logout</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
